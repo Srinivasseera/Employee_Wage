a3cbbbc04e23f2a8155b386e062b6245d37cddb2
Srinivasseera <srinivasseera7@gmail.com>
2024-05-30 12:17:27 +0530

    Adding the Employee Presence

---
+++ Employee_Wage/Emplyoee_Presence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage
+{
+    public class Employee
+    {
+        public void Print_Presence()
+        {
+            int Is_Present = 1;
+            Random random = new Random();
+            int number = random.Next(0, 2);
+
+            if (number == Is_Present)
+            {
+                Console.WriteLine("employee is present");
+
+
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+
+
+    public class Employee_Presence
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Print_Presence();
+       
+        }
+    }
+}
